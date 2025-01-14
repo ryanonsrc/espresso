def64bff921065aaec6c9e9cd9d3acd1b693a3d6
Ryan Delucchi <ryan.delucchi@fp.engineering>
2025-01-14 00:34:11 -0800

    minor cleanup and added visualization

--- src/main/scala/io/nary/espresso/defs.scala
+++ src/main/scala/io/nary/espresso/defs.scala
@@ -3,8 +3,8 @@ package io.nary.espresso
 import cats.data.{Kleisli, ValidatedNel}
 
 object defs:
-  type Term[E, A] = ValidatedNel[E, A]
+  type In[E, A] = Expr[E, Any, A]  // Values Read in from weakly-typed source to Expression
+  type Expr[E, A, B] = Kleisli[[α] =>> Term[E, α], A, B]  // String-typed values to Term
+  type Term[E, A] = ValidatedNel[E, A] // A Term is simply a validated expression result 
 
-  // Using type lambda syntax in Scala 3
-  type Expr[E, A, B] = Kleisli[[α] =>> Term[E, α], A, B]
-  type In[E, A] = Expr[E, Any, A]
+  
